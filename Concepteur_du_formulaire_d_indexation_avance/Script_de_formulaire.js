a3662f7bbee0bc8f4726571bf15628972984ad59
Roch DESCHASEAUX <roch.deschaseaux@mafassurances.onmicrosoft.com>
2023-11-07 14:59:45 +0100

    fichiers javascript de base pour l'étape de formulaire d'indexation de document

---
+++ Concepteur_du_formulaire_d_indexation_avance/Script_de_formulaire.js
@@ -0,0 +1,50 @@
+/*************************************************************************
+*
+* Form Script
+* 
+* Custom code for the form and its elements can be written in this script.
+* 
+* The code is defined and is being used as an Asynchronous Module 
+* Definition (AMD) javascript module. Any external JS framework, libary 
+* or other JS code that supports the AMD module system can be loaded 
+* and used. The loading of the modules is being handled by RequireJS. 
+*
+* Configuring and specifying each external module can be done with the 
+* require.config object of RequireJS to be used as dependencies with 
+* the AMD define() function below.
+* 
+* For example: To include jQuery v2.2.4 the following configuration
+* can be used. It will define a module named 'jquery' along with the 
+* a specific URL of where it can be fetched from. 'jquery' module will
+* then be available to be loaded and used as a dependency in the form 
+* script module.
+*
+* require.config({
+*     paths: {
+*         react: "//unpkg.com/react@16/umd/react.development",
+*         jquery: "//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery",        
+*     }
+* });
+*
+* More information about RequireJS configuration can be found in its 
+* official website:
+* https://requirejs.org/
+*
+* The Form Script module will be imported and evaluated using the 
+* requireJS module system each time the form will be displayed 
+* at runtime.
+* 
+* More information on how to write AMD modules can 
+* be found in the documentation of its official specification: 
+* https://github.com/amdjs/amdjs-api
+* 
+* The 'it' object dependency, implements the API of the form and 
+* should therefore be always included in the dependencies.
+*
+*************************************************************************/
+
+define("formScript", ["ctx"], function (ctx) {
+  // Any custom code 
+
+  return {};
+});
